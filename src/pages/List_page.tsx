04bd1a4e79848887ecf83fd527283de79a6aae8c
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2026-01-05 23:55:41 +0900

    Changes to be committed: 복사본을 만들어서 fix #2 	modified:   src/pages/List_page.tsx

--- src/pages/List_page.tsx
+++ src/pages/List_page.tsx
@@ -4,14 +4,14 @@ import ItemCard from '../components/ItemCard';
 
 
 const List_Page = forwardRef<ListHandle>((_props, ref) => {
-    contents.reverse();
+    const reversed_list = [...contents].reverse();
     const content_count = contents.length;
     const [searchTerm, setSearchTerm] = useState("")
     useImperativeHandle(ref, () => ({ setSearch: (v: string) => setSearchTerm(v) }));
     
     return (
         <div>
-            {contents.map((item, idx) => (
+            {reversed_list.map((item, idx) => (
                 (item.title.toLowerCase().includes(searchTerm.toLowerCase()) || item.description.toLowerCase().includes(searchTerm.toLowerCase()))
                 ? <ItemCard {...item} index={content_count-idx-1}></ItemCard>
                 : null // 삼향 연산자로 if 없이 검색 필터 구현
